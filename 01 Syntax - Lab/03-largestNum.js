e5d05d8c278029e152255af6e6db2feee7e872d9
theTrackSuitDev <dimiro89@gmail.com>
2023-09-01 01:00:36 +0300

    03 Largest Number

---
+++ 01 Syntax - Lab/03-largestNum.js
@@ -0,0 +1,7 @@
+function largest(numOne, numTwo, numThree) {
+    let biggestNum = Math.max(numOne, numTwo, numThree);
+
+    console.log(`The largest number is ${biggestNum}.`);
+}
+
+largest(-3, -5, -22.5);
