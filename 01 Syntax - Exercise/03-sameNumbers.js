9a72aeb2508612213738bbc1cbe10c6c70ff0cfb
theTrackSuitDev <dimiro89@gmail.com>
2023-09-01 16:35:37 +0300

    03 Same Numbers

---
+++ 01 Syntax - Exercise/03-sameNumbers.js
@@ -0,0 +1,20 @@
+function sameNumbers(num) {
+    
+    numArray = num.toString().split('');
+    let areSame = true;
+    for (let i = 0; i < numArray.length - 1; i++) {
+        const element = numArray[i];
+        if (element != numArray[i + 1]) {
+            areSame = false;
+            break;
+        }
+    }
+
+    numArray = numArray.map(Number);
+    let sum = numArray.reduce((a, b) => a + b, 0);
+
+    console.log(areSame);
+    console.log(sum);
+}
+
+sameNumbers(1234);
